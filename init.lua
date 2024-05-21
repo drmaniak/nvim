9913c264686fd4d55cf4c6cf4ecfee57dcfdaf47
Manick Vennimalai <manick.vennimalai@gmail.com>
2024-05-21 23:13:39 +0100

    Updated config, created lua/plugins.lua

--- init.lua
+++ init.lua
@@ -25,46 +25,12 @@ vim.opt.rtp:prepend(lazypath)
 
 
 -- Create tables for plugins and opts --
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python", "go" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  },
-
-}
-
 local opts = {}
 
 
 
 -- Load Lazy with Plugins --
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 -- Other Plugin Setup --
 -- Catppuccin --
@@ -84,7 +50,7 @@ local builtin  = require("telescope.builtin")
 map("n", "<leader>ff", builtin.find_files, {})
 map("n", "<leader>fg", builtin.live_grep, {})
 
-
+map("n", "<C-n>", ":Neotree toggle filesystem left<CR>")
 
 
 
