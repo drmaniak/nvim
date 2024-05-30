c0047930ac50cf3e464e6aaf7d6486c16ab287be
Manick Vennimalai <manick.vennimalai@gmail.com>
2024-05-31 00:04:43 +0100

    Added snippets

--- lua/helpers/keys.lua
+++ lua/helpers/keys.lua
@@ -1,24 +1,22 @@
 local M = {}
 
 M.map = function(mode, lhs, rhs, desc, opts)
-  -- default options --
-  local options = { silent = true, desc = desc }
+	-- default options --
+	local options = { silent = true, desc = desc }
 
-  -- append custom opts --
-  if opts then
-    options = vim.tbl_extend("force", options, opts) 
-  end
+	-- append custom opts --
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
 
-
-  -- Set keymap with options --
-  vim.keymap.set(mode, lhs, rhs, options)
-
+	-- Set keymap with options --
+	vim.keymap.set(mode, lhs, rhs, options)
 end
 
 M.set_leader = function(key)
-  vim.g.mapleader = key
-  vim.g.maplocalleader = key
-  M.map({"n", "v"}, key, "<nop>")
+	vim.g.mapleader = key
+	vim.g.maplocalleader = key
+	M.map({ "n", "v" }, key, "<nop>")
 end
 
 return M
