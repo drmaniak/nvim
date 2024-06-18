efb0165cb3743ead43f4190cb457aedb2e16c8d3
Manick Vennimalai <manick.vennimalai@gmail.com>
2024-06-18 23:50:46 +0100

    Updated Conform timeout, deactivated none-ls

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -10,14 +10,16 @@ return {
 
 			-- Specify the formatters by file type
 			formatters_by_ft = {
-				python = function(bufnr)
-					if require("conform").get_formatter_info("ruff_format", bufnr).available then
-						return { "ruff_format", "black" }
-					else
-						return { "isort", "black" }
-					end
-				end,
 
+				-- python = function(bufnr)
+				-- 	if require("conform").get_formatter_info("ruff_format", bufnr).available then
+				-- 		return { "ruff_format", "black" }
+				-- 	else
+				-- 		return { "isort", "black" }
+				-- 	end
+				-- end,
+
+				python = { "isort", "black", "ruff_format" },
 				lua = { "stylua" },
 				markdown = { "prettier" },
 				bash = { "beautysh" },
@@ -25,7 +27,7 @@ return {
 
 			-- Specify format on save
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 5000,
 				lsp_fallback = true,
 			},
 		})
@@ -34,7 +36,7 @@ return {
 			conform.format({
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 500,
+				timeout_ms = 5000,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,
