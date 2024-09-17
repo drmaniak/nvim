6eeb19c779093ae952b0b5e53bf2f8ed40bae5a1
Manick Vennimalai <manick.vennimalai@gmail.com>
2024-09-17 12:03:57 +0100

    Updated catppuccin for WinSeparator and dim_inactive

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,72 +1,78 @@
 return {
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000,
-  config = function()
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			term_colors = false,
+			transparent_background = false,
+			dim_inactive = {
+				enabled = true,
+				shade = "light",
+				percentage = 0.03,
+			},
 
-    require("catppuccin").setup({
-      transparent_background = false,
+			opts = {
 
-      opts = {
-        integrations = {
-          aerial = true,
-          alpha = true,
-          cmp = true,
-          dashboard = true,
-          flash = true,
-          gitsigns = true,
-          headlines = true,
-          illuminate = true,
-          indent_blankline = { enabled = true },
-          leap = true,
-          lsp_trouble = true,
-          mason = true,
-          markdown = true,
-          mini = true,
-          native_lsp = {
-            enabled = true,
-            underlines = {
-              errors = { "undercurl" },
-              hints = { "undercurl" },
-              warnings = { "undercurl" },
-              information = { "undercurl" },
-            },
-          },
-          navic = { enabled = true, custom_bg = "lualine" },
-          neotest = true,
-          neotree = true,
-          noice = true,
-          notify = true,
-          semantic_tokens = true,
-          telescope = true,
-          treesitter = true,
-          treesitter_context = true,
-          -- which_key = true
-        }
-      },
+				integrations = {
+					aerial = true,
+					alpha = true,
+					cmp = true,
+					dashboard = true,
+					flash = true,
+					gitsigns = true,
+					headlines = true,
+					illuminate = true,
+					indent_blankline = { enabled = true },
+					leap = true,
+					lsp_trouble = true,
+					mason = true,
+					markdown = true,
+					mini = true,
+					native_lsp = {
+						enabled = true,
+						underlines = {
+							errors = { "undercurl" },
+							hints = { "undercurl" },
+							warnings = { "undercurl" },
+							information = { "undercurl" },
+						},
+					},
+					navic = { enabled = true, custom_bg = "lualine" },
+					neotest = true,
+					neotree = true,
+					noice = true,
+					notify = true,
+					semantic_tokens = true,
+					telescope = true,
+					treesitter = true,
+					treesitter_context = true,
+					-- which_key = true
+				},
+			},
 
-      color_overrides = {
-        all = {
-          text = "#ffffff",
-        },
-        mocha = {
-          base = "#14122a"
-        },
-        frappe = {},
-        macchiato = {},
-        latte = {},
-      },
+			color_overrides = {
+				all = {
+					text = "#ffffff",
+				},
+				mocha = {
+					base = "#14122a",
+				},
+				frappe = {},
+				macchiato = {},
+				latte = {},
+			},
 
-      custom_highlights = function(colors)
-        return {
-          NeoTreeNormal = { bg = "#15132f" },
-          NeoTreeNormalNC = { bg = "#15132f" },
-        }
-      end
+			custom_highlights = function(colors)
+				return {
+					WinSeparator = { fg = colors.lavender, bg = colors.none },
+					NeoTreeWinSeparator = { fg = colors.lavender, bg = colors.none },
+					NeoTreeNormal = { bg = "#15132f" },
+					NeoTreeNormalNC = { bg = "#15132f" },
+				}
+			end,
+		})
 
-    })
-
-    vim.cmd.colorscheme "catppuccin-mocha"
-
-  end
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
