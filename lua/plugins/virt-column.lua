3c12cbe9d522667271a03514890f5a3cd8ff99c2
Manick Vennimalai <manick.vennimalai@gmail.com>
2025-02-23 23:04:38 +0000

    Updated virt-column to add extra vertical line

--- lua/plugins/virt-column.lua
+++ lua/plugins/virt-column.lua
@@ -1,8 +1,8 @@
 return {
-	"lukas-reineke/virt-column.nvim",
-	opts = {
-		char = { "", "" },
-		virtcolumn = "87, 88",
-		highlight = { "NvimStringSpecial", "NvimStringSpecial" },
-	},
+  "lukas-reineke/virt-column.nvim",
+  opts = {
+    char = { "", "" },
+    virtcolumn = "87, 88, 99, 100",
+    highlight = { "NvimStringSpecial", "NvimStringSpecial" },
+  },
 }
