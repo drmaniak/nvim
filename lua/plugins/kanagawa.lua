77e6d483b9f29d234c35a4f2f62cc42c59b3c9a3
Manick Vennimalai <manick.vennimalai@gmail.com>
2025-05-06 11:09:42 +0100

    End of an Era... Catppuccin -> Kanagawa

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,34 +1,38 @@
 return {
-  "rebelot/kanagawa.nvim",
-  config = function()
-    require("kanagawa").setup({
-      complie = false,
-      undercurl = true,
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,
-      dimInactive = false,
-      terminalColors = true,
-      colors = { -- add/modify theme and palette colors
-        palette = {},
-        theme = {
-          wave = {},
-          lotus = {},
-          dragon = {},
-          all = {},
-        },
-      },
-      overrides = function(colors) -- add/modify highlights
-        return {}
-      end,
-      theme = "wave", -- Load "wave" theme when 'background' option is not set
-      background = { -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus",
-      },
-    })
-  end,
+	"rebelot/kanagawa.nvim",
+	opts = {
+		complie = false,
+		undercurl = true,
+		commentStyle = { italic = true },
+		functionStyle = {},
+		keywordStyle = { italic = true },
+		statementStyle = { bold = true },
+		typeStyle = {},
+		transparent = true,
+		dimInactive = false,
+		terminalColors = true,
+		colors = { -- add/modify theme and palette colors
+			palette = {},
+			theme = {
+				wave = {},
+				lotus = {},
+				dragon = {},
+				all = {
+					ui = {
+						bg_gutter = "none",
+					},
+				},
+			},
+		},
+		overrides = function(colors) -- add/modify highlights
+			return {
+				LineNr = { fg = colors.palette.dragonGray2 },
+			}
+		end,
+		theme = "wave", -- Load "wave" theme when 'background' option is not set
+		background = { -- map the value of 'background' option to a theme
+			dark = "wave", -- try "dragon" !
+			light = "lotus",
+		},
+	},
 }
